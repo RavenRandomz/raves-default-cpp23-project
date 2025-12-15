d18c3f8a0946ee989a3f8d6067a5f3095c6afe2a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-15 11:05:00 -0600

    check: Embedding files

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,18 @@ import game.FiveWord;
 import word.AnotherTestWord;
 import word.Word;
 import game.Dictionary;
+#include <string>
+#include <iostream>
 // Rare using moment :)
 using namespace wordlr;
 int main()
 {
+    const char kDictionaryTest[] = {
+    #embed "../res/valid-wordle-words.txt"
+    ,'\0'
+    };
+    std::cout <<kDictionaryTest;
+
     game::Dictionary test{};
     return 0;
 }
