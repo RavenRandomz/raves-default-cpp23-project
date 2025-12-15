bbf79e77110eae19c4161510c047b381f89f90b8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-15 10:33:51 -0600

    check: Compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 import derp;
 import word.AnotherTestWord;
 import word.Word;
+import game.Dictionary;
 
-
+// Rare using moment :)
+using namespace wordlr;
 int main()
 {
+    game::Dictionary test{};
     return 0;
 }
